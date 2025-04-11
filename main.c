23b00afc04f2c0eb8a5d6047fe5e9bfbb4270710
SaigerMaine <pavel.gorenflot2006@gmail.com>
2025-04-11 16:50:45 +0200

    ordre

--- main.c
+++ main.c
@@ -37,7 +37,7 @@ float verifloat(){
 
 
 
-char verifs(){
+char* verifs(){
     float err=0;
     char d[100];
     while (scanf("%s",&err)!= 1 ){
@@ -72,6 +72,28 @@ typedef struct{     // create class special skills
     Character p3;
 }Equipe;
 
+int mode(){
+    //  n=1 facile n=2 difficile n=3 infernal n=4 1v1
+    int d=0;
+    int n = 0;
+    printf("tape 1 pour jouer contre l'ordinateur \ntape 2 pour jouer contre un ami\n");
+   while (d!= 1 && d!= 2){
+    d = verifint();
+   }
+    if (d == 1){
+        printf(" tape 1 mode facile \n tape 2 mode difficile \n tape 3 mode infernal\n");
+        n= verifint();
+        while (n!=1 && n!=2 && n!=3){
+            printf("t a pas taper 1 2 ou 3 mon reuf peut tu reessayer");
+            n= verifint();
+        }
+    }
+    if(d==2){
+        n=4;
+    }
+    return n;
+}
+
 int main(){
 
 Skill Rugissement;      // initialising special skill : Rugissement -40% DEF
@@ -98,37 +120,7 @@ Loup.ch_skill2 = Hurlement_de_chasse;
 
  printf("Personnage : Loup\n");
  printf("HP : %f\nATK : %f\nDEF : %f\nRES : %f\nSPD : %f\nskill 1 : %s\nskill 2 : %s\n", Loup.MaxHP, Loup.ATK, Loup.DEF, Loup.RES, Loup.SPD, Loup.ch_skill1.name, Loup.ch_skill2.name);
-    return 0;
-}
-
-
-
-int mode(){
-    //  n=1 facile n=2 difficile n=3 infernal n=4 1v1
-    int d=0;
-    int n = 0;
-    printf("tape 1 pour jouer contre l'ordinateur \ntape 2 pour jouer contre un ami\n");
-   while (d!= 1 && d!= 2){
-    d = verifint();
-   }
-    if (d == 1){
-        printf(" tape 1 mode facile \n tape 2 mode difficile \n tape 3 mode infernal\n");
-        n= verifint();
-        while (n!=1 && n!=2 && n!=3){
-            printf("t a pas taper 1 2 ou 3 mon reuf peut tu reessayer");
-            n= verifint();
-        }
-    }
-    if(d==2){
-        n=4;
-    }
-    return n;
-}
-
-
-
-
-int main(){
+ 
     printf("tape 2 si tu veux pas jouer\ntape un autre chiffre pour jouer\n");
     if (verifint() == 2 ){
         printf("bah chao bye bye");
@@ -137,6 +129,3 @@ int main(){
   mode();
   return 0;
 }
-
-
-
