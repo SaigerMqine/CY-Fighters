832eb49597afcff91d8de755333cf64270fea43d
xotu6066 <aymericcasteret@orange.fr>
2025-04-11 16:19:57 +0200

    fonction mode

--- main.c
+++ main.c
@@ -103,5 +103,40 @@ Loup.ch_skill2 = Hurlement_de_chasse;
 
 
 
+int mode(){
+    //  n=1 facile n=2 difficile n=3 infernal n=4 1v1
+    int d=0;
+    int n = 0;
+    printf("tape 1 pour jouer contre l'ordinateur \ntape 2 pour jouer contre un ami\n");
+   while (d!= 1 && d!= 2){
+    d = verifint();
+   }
+    if (d == 1){
+        printf(" tape 1 mode facile \n tape 2 mode difficile \n tape 3 mode infernal\n");
+        n= verifint();
+        while (n!=1 && n!=2 && n!=3){
+            printf("t a pas taper 1 2 ou 3 mon reuf peut tu reessayer");
+            n= verifint();
+        }
+    }
+    if(d==2){
+        n=4;
+    }
+    return n;
+}
+
+
+
+
+int main(){
+    printf("tape 2 si tu veux pas jouer\ntape un autre chiffre pour jouer\n");
+    if (verifint() == 2 ){
+        printf("bah chao bye bye");
+        return 0  ; 
+    }
+  mode();
+  return 0;
+}
+
 
 
