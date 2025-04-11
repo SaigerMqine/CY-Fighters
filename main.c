5012baf510857eb25a71f80922eb18674857202f
SaigerMaine <pavel.gorenflot2006@gmail.com>
2025-04-11 15:25:16 +0200

    Update main.c

--- main.c
+++ main.c
@@ -20,13 +20,13 @@ typedef struct{     // create class characters
 
 int main(){
 
-Skill Rugissement;      // initialising special skill : Rugissement 
+Skill Rugissement;      // initialising special skill : Rugissement -40% DEF
 strcpy(Rugissement.name, "Rugissement");
 Rugissement.value = 0.6;
 Rugissement.duration = 1;
 Rugissement.cd = 2;
 
-Skill Hurlement_de_chasse;      // initialising special skill :                                  Hurlement de chasse 
+Skill Hurlement_de_chasse;      // initialising special skill : Hurlement de chasse +30% DEF
 strcpy(Hurlement_de_chasse.name, "Hurlement de chasse");
 Hurlement_de_chasse.value = 1.3;
 Hurlement_de_chasse.duration = 1;
@@ -40,7 +40,7 @@ Loup.DEF = 18;
 Loup.RES = 10;
 Loup.SPD = 107;
 Loup.ch_skill1 = Rugissement;
-Loup.ch_skill2 = Hurlement_de_chasse; // ATK team +30%
+Loup.ch_skill2 = Hurlement_de_chasse;
 
  printf("Personnage : Loup\n");
  printf("HP : %f\nATK : %f\nDEF : %f\nRES : %f\nSPD : %f\nskill 1 : %s\nskill 2 : %s\n", Loup.MaxHP, Loup.ATK, Loup.DEF, Loup.RES, Loup.SPD, Loup.ch_skill1.name, Loup.ch_skill2.name);
