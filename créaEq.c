734faea02bb27e2f6662c6b867881ec34e4c4422
SaigerMaine <pavel.gorenflot2006@gmail.com>
2025-04-12 22:02:12 +0200

    persos dans creaequipe

--- créaEq.c
+++ créaEq.c
@@ -1,4 +1,30 @@
-Equipe créationéquipe(les characters){
+typedef struct{     // create class special skills
+    char name[50];
+    float value;
+    float duration;
+    float cd;
+}Skill;
+
+typedef struct{     // create class characters 
+    float MaxHP;
+    float ActHP;
+    float ATK;
+    float DEF;
+    float RES;
+    float SPD;
+    Skill ch_skill1;
+    Skill ch_skill2;
+}Character;
+
+typedef struct{     // create class special skills
+    char name[50];
+    Character p1;
+    Character p2;
+    Character p3;
+}Team;
+
+
+Team MakeTeam(les characters){
 int n;
 int a;
 int b;
@@ -7,27 +33,27 @@ printf("Personnage : Loup\n");
     printf("choisissez un perso parmi ci-dessus en taper 1,2,3,4,5,6 respectivement");
     n=verifint();
     if(n==1){
-    Equipe.p1 = c1
+    Team.p1 = c1
     a=1;  
     }
     if(n==2){
-    Equipe.p1 = c2
+    Team.p1 = c2
     a=2;   
     }
     if(n==3){
-    Equipe.p1 = c3
+    Team.p1 = c3
     a=3;    
     }
     if(n==4){
-    Equipe.p1 = c4
+    Team.p1 = c4
     a=4;   
     }
     if(n==5){
-    Equipe.p1 = c5
+    Team.p1 = c5
     a=5;    
     }
     if(n==6){
-    Equipe.p1 = c6
+    Team.p1 = c6
     a=6;   
     }
 printf("choisissez un perso parmi ci-dessus en taper 1,2,3,4,5,6 respectivement");
@@ -37,27 +63,27 @@ printf("choisissez un perso parmi ci-dessus en taper 1,2,3,4,5,6 respectivement"
     n=verifint();    
     }
     if(n==1){
-        Equipe.p2 = c1
+        Team.p2 = c1
         b=1;    
     }
     if(n==2){
-        Equipe.p2 = c2
+        Team.p2 = c2
         b=2;    
     }
     if(n==3){
-        Equipe.p2 = c3
+        Team.p2 = c3
         b=3;    
     }
     if(n==4){
-        Equipe.p2 = c4
+        Team.p2 = c4
         b=4;    
     }
     if(n==5){
-        Equipe.p2 = c5
+        Team.p2 = c5
         b=5;    
     }
     if(n==6){
-        Equipe.p2 = c6
+        Team.p2 = c6
         b=6;    
     }
     printf("choisissez un perso parmi ci-dessus en taper 1,2,3,4,5,6 respectivement");
@@ -67,22 +93,22 @@ printf("choisissez un perso parmi ci-dessus en taper 1,2,3,4,5,6 respectivement"
          n=verifint();   
     }
     if(n==1){
-        Equipe.p3 = c1
+        Team.p3 = c1
     }
     if(n==2){
-        Equipe.p3 = c2  
+        Team.p3 = c2  
     }
     if(n==3){
-        Equipe.p3 = c3    
+        Team.p3 = c3    
     }
     if(n==4){
-        Equipe.p3 = c4    
+        Team.p3 = c4    
     }
     if(n==5){
-        Equipe.p3 = c5
+        Team.p3 = c5
     }
     if(n==6){
-        Equipe.p3 = c6
+        Team.p3 = c6
     }
-    return Equipe;
+    return Team;
 }
