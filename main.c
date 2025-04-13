df2c2a0b090597db6443b16df529922f40e709d2
xotu6066 <aymericcasteret@orange.fr>
2025-04-13 16:49:49 +0200

    debut du jeu

--- main.c
+++ main.c
@@ -23,6 +23,24 @@ int mode(){
     }
     return n;
 }
+void ordifacile(){           //fonction pour jouer contre l'ordi mais c'est le debut et il faut mettre les bons perso dans le tableau 
+    character tabcharacter[6] 
+    float tab[6]
+    choixordiequipe()
+    while(p1.ActHP != 0 && p2.ActHP != 0 && p3.ActHP != 0 ){
+        for (int i=0 i<6 i++){
+            tab[i]+= tabcharacter[i].SPD
+            if (tab[i] >= 100){       // 100 c'est une valeur temporaire  
+                if (i<3){
+                AffichageDuPerso()   // dans l'affichage on mais aussi les verifint
+                }
+                else{
+                    // ici on met quand l'ordi attaque 
+                }
+            }      
+        }
+    }
+}       // je vous laisse l'ameliorer normalement le debut sera toujours pareil pour chaque ordi et pour le 1 contre 1 il faudra juste changer ChoixOrdiEquipe aussi j ai mis des fonction mais je sais pas quelle nom vous voulez mettre 
 
 int main(){
 
@@ -58,6 +76,19 @@ int main(){
         printf("bah chao bye bye");
         return 0  ; 
     }
-    mode();
+    switch(mode()){ // je sais pas si c'est bon ou pas comme on a pas les fonctions mais je le met quand meme 
+        case 1:
+        ordifacile()
+        break;
+        case 2 :
+        ordimoyen()
+        break;
+        case 3 :
+        ordidifficile()
+        break;
+        case 4 :
+        1vs1()
+        break;
+    }
     return 0;
 
