aa3dd4ca43a89622033fe0b5f1b2e39ae86b38c3
timothee23-gh <timothee.renouf23@gmail.com>
2025-04-28 17:02:07 +0200

    Update début ordifac

--- combat.c
+++ combat.c
@@ -1,17 +1,18 @@
 void ordifacile(){           //fonction pour jouer contre l'ordi mais c'est le debut et il faut mettre les bons perso dans le tableau 
-    character tabcharacter[6];
-    int tab[6];
     MakeTeam(les characters);
     MakeTeambot(les characters);
-    while(Team1.p1.ActHP > 0 || Team1.p2.ActHP > 0 || Team1.p3.ActHP > 0 && Team2.p1.ActHP > 0 || Team2.p2.ActHP > 0 || Team2.p3.ActHP > 0){
-        for (int i=0 i<6 i++){
-            tab[i]+= tabcharacter[i].SPD
-            if (tab[i] >= 100){       // 100 c'est une valeur temporaire  
-                if (i<3){
-                AffichageDuPerso()   // dans l'affichage on mais aussi les verifint
-                }
-                else{
-                    // ici on met quand l'ordi attaque 
+    while(Team1.p1.ActHP > 0 && Team1.p2.ActHP > 0 && Team1.p3.ActHP > 0 || Team2.p1.ActHP > 0 && Team2.p2.ActHP > 0 && Team2.p3.ActHP > 0){
+        Team1.p1.ATK_bar=Team1.p1.ATK_bar+Team1.p1.SPD;
+        Team1.p2.ATK_bar=Team1.p2.ATK_bar+Team1.p2.SPD;
+        Team1.p3.ATK_bar=Team1.p3.ATK_bar+Team1.p3.SPD;
+        Team2.p1.ATK_bar=Team2.p1.ATK_bar+Team2.p1.SPD;
+        Team2.p2.ATK_bar=Team2.p2.ATK_bar+Team2.p2.SPD;
+        Team2.p3.ATK_bar=Team2.p3.ATK_bar+Team2.p3.SPD;
+                if (Team1.p1.ATK_bar >=500 ||   Team1.p2.ATK_bar >= 500 || Team1.p3.ATK_bar >= 500 || Team2.p1.ATK_bar >= 500 || Team2.p2.ATK_bar >= 500 || Team2.p3.ATK_bar >= 500){
+                
+                    
+                    AffichageDuPerso()   // dans l'affichage on met aussi les verifint pour choix de l'atk
+                
                 }
             }      
         }
