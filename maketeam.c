03bfd54c05bc0a567b4b17ff778bceb42cbec1e5
timothee23-gh <timothee.renouf23@gmail.com>
2025-04-28 16:03:25 +0200

    maketeam pour le bot fait
    maketeam pour le bot fait

--- maketeam.c
+++ maketeam.c
@@ -26,42 +26,42 @@ typedef struct{     // create class special skills
 }Team;
 
 Team MakeTeam(les characters){
-int n;
-int a;
-int b;
-printf("Personnage : Loup\n");
- printf("HP : %f\nATK : %f\nDEF : %f\nRES : %f\nSPD : %f\nskill 1 : %s\nskill 2 : %s\n", Loup.MaxHP, Loup.ATK, Loup.DEF, Loup.RES, Loup.SPD, Loup.ch_skill1.name, Loup.ch_skill2.name);
+    int n;
+    int a;
+    int b;
+    printf("Personnage : Loup\n");
+    printf("HP : %f\nATK : %f\nDEF : %f\nRES : %f\nSPD : %f\nskill 1 : %s\nskill 2 : %s\n", Loup.MaxHP, Loup.ATK, Loup.DEF, Loup.RES, Loup.SPD, Loup.ch_skill1.name, Loup.ch_skill2.name);
     printf("choisissez un perso parmi ci-dessus en taper 1,2,3,4,5,6 respectivement");
     n=verifint();
     if(n==1){
-    Team.p1 = c1
-    a=1;  
+        Team.p1 = c1
+        a=1;  
     }
     if(n==2){
-    Team.p1 = c2
-    a=2;   
+        Team.p1 = c2
+        a=2;   
     }
     if(n==3){
-    Team.p1 = c3
-    a=3;    
+        Team.p1 = c3
+        a=3;    
     }
     if(n==4){
-    Team.p1 = c4
-    a=4;   
+        Team.p1 = c4
+        a=4;   
     }
     if(n==5){
-    Team.p1 = c5
-    a=5;    
+        Team.p1 = c5
+        a=5;    
     }
     if(n==6){
-    Team.p1 = c6
-    a=6;   
+        Team.p1 = c6
+        a=6;   
     }
-printf("choisissez un perso parmi ci-dessus en taper 1,2,3,4,5,6 respectivement");
+    printf("choisissez un perso parmi ci-dessus en taper 1,2,3,4,5,6 respectivement");
     n=verifint();
     while (n==a){
-    printf("choisissez un perso parmi ci-dessus en taper 1,2,3,4,5,6 respectivement");
-    n=verifint();    
+        printf("choisissez un perso parmi ci-dessus en taper 1,2,3,4,5,6 respectivement");
+        n=verifint();    
     }
     if(n==1){
         Team.p2 = c1
@@ -90,8 +90,8 @@ printf("choisissez un perso parmi ci-dessus en taper 1,2,3,4,5,6 respectivement"
     printf("choisissez un perso parmi ci-dessus en taper 1,2,3,4,5,6 respectivement");
     n=verifint();
     while (n==a || n==b){
-    printf("choisissez un perso parmi ci-dessus en taper 1,2,3,4,5,6 respectivement");
-         n=verifint();   
+        printf("choisissez un perso parmi ci-dessus en taper 1,2,3,4,5,6 respectivement");
+        n=verifint();   
     }
     if(n==1){
         Team.p3 = c1
@@ -111,5 +111,86 @@ printf("choisissez un perso parmi ci-dessus en taper 1,2,3,4,5,6 respectivement"
     if(n==6){
         Team.p3 = c6
     }
+    return Team;
+}
+Team MakeTeambot(les characters){
+    int n;
+    int a;
+    int b;
+    n=(rand()%6)+1;
+    if(n==1){
+        Team.p1 = c1
+        a=1;  
+    }
+    if(n==2){
+        Team.p1 = c2
+        a=2;   
+    }
+    if(n==3){
+        Team.p1 = c3
+        a=3;    
+    }
+    if(n==4){
+        Team.p1 = c4
+        a=4;   
+    }
+    if(n==5){
+        Team.p1 = c5
+        a=5;    
+    }
+    if(n==6){
+        Team.p1 = c6
+        a=6;   
+    }
+    n=(rand()%6)+1;
+    while (n==a){
+        n=(rand()%6)+1    
+        }
+        if(n==1){
+            Team.p2 = c1
+            b=1;    
+        }
+        if(n==2){
+            Team.p2 = c2
+            b=2;    
+        }
+        if(n==3){
+            Team.p2 = c3
+            b=3;    
+        }
+        if(n==4){
+            Team.p2 = c4
+            b=4;    
+        }
+        if(n==5){
+            Team.p2 = c5
+            b=5;    
+        }
+        if(n==6){
+            Team.p2 = c6
+            b=6;    
+        }
+    n=(rand()%6)+1;
+    while (n==a || n==b){
+        n=(rand()%6)+1;   
+        }
+        if(n==1){
+            Team.p3 = c1
+        }
+        if(n==2){
+            Team.p3 = c2  
+        }
+        if(n==3){
+            Team.p3 = c3    
+        }
+        if(n==4){
+            Team.p3 = c4    
+        }
+        if(n==5){
+            Team.p3 = c5
+        }
+        if(n==6){
+            Team.p3 = c6
+        }
     return Team;
 }
