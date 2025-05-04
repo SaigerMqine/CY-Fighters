8e3b72baf8ce8b305190dc583e98539278bf3d4c
xotu6066 <aymericcasteret@orange.fr>
2025-05-04 19:18:25 +0200

    toujours pas fini

--- jeu_interface.c
+++ jeu_interface.c
@@ -69,13 +69,173 @@ typedef struct {
     printf("] |");
  }
  
+ void afficheeffet(){
+     // il faut le faire
+ }
+
+  
+ void afficheperso(character p1){
+     printf("|   %s",p1.name);
+     for(int i=strlen(p1.name);i<95-;i++){
+         printf(" ");
+     }
+     printf("|\n");
+     printf("|   TECHNIQUES SPECIALES");
+     for(int i=0;i<75-;i++){
+         printf(" ");
+     }
+     printf("|\n");
+     printf("|      |1| attaque normal(peut etre utiliser a chaque tour)                                            |");
+     if (p1.ch_skill1.cd == 0){
+         printf("|      |2|");
+         printf(" %s (duree:%d tours)",p1.ch_skill1.name ,p1.ch_skill1.duration);
+         for(int i=strlen(p1.ch_skill1.name);i<73;i++){
+         printf(" ");
+        }
+        printf("|\n");
+     }
+     else{
+         printf("|      |-|");
+         printf(" %s (recharge: %d duree:%d tours)",p1.ch_skill2.name,p1.ch_skill2.cd ,p1.ch_skill2.duration);
+         for(int i=strlen(p1.ch_skill1.name);i<61;i++){
+         printf(" ");
+        }
+        printf("|\n");
+     }
+     printf("|            %s",p1.ch_skill1.description);
+     for(int i=strlen(p1.ch_skill1.description);i<87;i++){
+         printf(" ");
+    }
+     printf("|\n");
+     
+          if (p1.ch_skill2.cd == 0){
+         printf("|      |3|");
+         printf(" %s (duree:%d tours)",p1.ch_skill2.name ,p1.ch_skill2.duration);
+         for(int i=strlen(p1.ch_skill2.name);i<73;i++){
+         printf(" ");
+        }
+        printf("|\n");
+     }
+     else{
+         printf("|      |-|");
+         printf(" %s (recharge: %d duree:%d tours)",p1.ch_skill2.name,p1.ch_skill2.cd ,p1.ch_skill2.duration);
+         for(int i=strlen(p1.ch_skill2.name);i<61;i++){
+             printf(" ");
+        }
+        printf("|\n");
+     }
+      printf("|            %s",p1.ch_skill1.description);
+     for(int i=strlen(p1.ch_skill1.description);i<87;i++){
+         printf(" ");
+    }
+     printf("|\n");
+     
+ }
+
+ 
  void affichageteam(Team t1){
-     printf("┌──[%s]",t1.name[]);// il faut faire les 2 team
+     printf("┌──[%s]",t1.name);// il faut faire les 2 team
      for(int i=strlen(t1.name);i<94-;i++){
          printf("─");
      }
-     printf("┐")
-     printf("\n| ")
-     affichenom()
+     printf("┐");
+     printf("\n| ");
+     affichenom(t1);
+     afficheeffet();
      hpsp(t1.p1,t1.p2,t1.p3);
+     printf("\n|");
+     for (int i=0;i<98;i++){
+         printf(" ");
+     }
+     printf("|");
  }
+ void affichagefin(){
+     printf("\n|");
+     for (int i=0;i<98;i++){
+         printf(" ");
+     }
+     printf("|\n");
+     printf(" ");
+     for (int i=0;i<98;i++){
+         printf("─");
+     }
+ }
+ 
+void affichage1v1(jeu j){ // fonction prochainperso a code il faut qu'elle renvoie le perso qui attaque   
+     if(j.t1.p1 == prochainperso()){
+         affichageteam(j.t2);
+         affichagefin();
+         printf("\n \n");
+         affichageteam(j.t1);
+         afficheperso(j.t1.p1);
+         affichagefin();
+         
+         
+     }
+     else if(j.t1.p2 == prochainperso()){
+         affichageteam(j.t2);
+         affichagefin();
+         printf("\n \n");
+         affichageteam(j.t1);
+         afficheperso(j.t1.p2);
+         affichagefin();
+         
+         
+     }
+     else if(j.t1.p3 == prochainperso()){
+         affichageteam(j.t2);
+         affichagefin();
+         printf("\n \n");
+         affichageteam(j.t1);
+         afficheperso(j.t1.p3);
+         affichagefin();
+         
+         
+     }
+     else if(j.t2.p1 == prochainperso()){
+         // on vera plus tard
+     }
+     else if(j.t2.p2 == prochainperso()){
+         
+     }
+     else if(j.t2.p3 == prochainperso()){
+         
+     }
+    
+}
+
+void affichageai(jeu j){
+     if(j.t1.p1 == prochainperso()){
+         affichageteam(j.t2);
+         affichagefin();
+         printf("\n \n");
+         affichageteam(j.t1);
+         afficheperso(j.t1.p1);
+         affichagefin();
+         
+         
+     }
+     else if(j.t1.p2 == prochainperso()){
+         affichageteam(j.t2);
+         affichagefin();
+         printf("\n \n");
+         affichageteam(j.t1);
+         afficheperso(j.t1.p2);
+         affichagefin();
+         
+         
+     }
+     else if(j.t1.p3 == prochainperso()){
+         affichageteam(j.t2);
+         affichagefin();
+         printf("\n \n");
+         affichageteam(j.t1);
+         afficheperso(j.t1.p3);
+         affichagefin();
+         
+         
+     }
+     else{
+         // on vera plus tard
+     }
+}
