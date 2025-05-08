76cbf2e60613306431b0ee5e33fdb1c77fcac7fe
timothee23-gh <timothee.renouf23@gmail.com>
2025-05-08 16:11:39 +0200

    Update calcul_des_dégats_pris.c

--- calcul_des_dégats_pris.c
+++ calcul_des_dégats_pris.c
@@ -1,2 +1,11 @@
-float calcul_des_dégats_pris(persoattaqué){
-    
+float calcul_des_dégats_pris(character p1,character p2){ //p1=perso attaqué, p2=perso qui attaque
+    int i=0;
+    i=(rand()%100)+1;
+    if(i<=p1.RES){
+        return p1.actHP;
+    }
+    else{
+        p1.actHP=p1.actHP-p2.ATK*(1-(p1.DEF/100));
+        return p1.actHP;
+    }
+}
