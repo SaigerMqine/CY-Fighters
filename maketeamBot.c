fe9095b70afe383ac3335e16cc0ac07ba4bc6418
SaigerMaine <pavel.gorenflot2006@gmail.com>
2025-05-08 14:37:47 +0200

    création perso 1, affichage perso et choix persos

---
+++ maketeamBot.c
@@ -0,0 +1,6 @@
+Team MakeTeam(Character characters[], int nb) {
+    Team team;
+    printf("Entrez le nom de votre équipe : ");
+    team.name = verifname
+    int choisis[3] = {-1, -1, -1};
+    int i = 0;
