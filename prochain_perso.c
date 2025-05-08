5d49fa1c7a324020a7f4da577b64a65e149dd6d5
xotu6066 <aymericcasteret@orange.fr>
2025-05-08 17:11:17 +0200

    Create prochain_perso.c

---
+++ prochain_perso.c
@@ -0,0 +1,14 @@
+Character maxi(Character p1,Character p2){
+    if(p1.ATK_bar>=P2.ATK_bar){
+        return p1
+    }
+    else{
+        return p2
+    }
+}
+
+Character prochainperso(Jeu j){
+    if(maxi(maxi(maxi(j.t1.p1,j.t1.p2),maxi(j.t1.p3,j.t2.p1)),maxi(j.t2.p2,j.t2.p3)).ATK_bar>500){
+        return maxi(maxi(maxi(j.t1.p1,j.t1.p2),maxi(j.t1.p3,j.t2.p1)),maxi(j.t2.p2,j.t2.p3));
+    }
+}
