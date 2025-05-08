67866249d96d41ecd4b456e21d9fc390cfac46cf
SaigerMaine <pavel.gorenflot2006@gmail.com>
2025-05-08 14:07:31 +0200

    struct

--- maketeam.h
+++ maketeam.h
@@ -3,4 +3,32 @@
 #include <string.h>
 #include <time.h>
 
+typedef struct{     // create class special skills
+    char name[50];
+    float value;
+    float duration;
+    float cd;
+    char description[100];
+}Skill;
+
+typedef struct{     // create class characters 
+    char name[50];
+    float MaxHP;
+    float ActHP;
+    float ATK;
+    float DEF;
+    float RES;
+    float SPD;
+    int ATK_bar;
+    Skill ch_skill1;
+    Skill ch_skill2;
+}Character;
+
+typedef struct{     // create class special skills
+    char name[50];
+    Character p1;
+    Character p2;
+    Character p3;
+}Team;
+
 Team MakeTeam(les characters);
