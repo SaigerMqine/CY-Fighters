96c3d0e4b24ddd5ef64a7cad4276d5c2fe801420
xotu6066 <aymericcasteret@orange.fr>
2025-05-09 16:14:28 +0200

    Update maketeamBot.c

--- maketeamBot.c
+++ maketeamBot.c
@@ -9,6 +9,12 @@ Team MakeTeam(Character characters[], int nb) {
         printf("Choisissez le personnage %d (1 à %d) : ", i + 1, nb);
         int choix = verifint() - 1;
 
+        
+        while (choix!= 1 && choix!= 2 && choix!= 3 && choix!= 4 && choix!= 5 && choix!= 0){
+            printf("Erreur, Tape  un perso valide \n");
+            choix = verifint()-1;
+        }   
+
         int deja_choisi = 0;
         for (int k = 0; k < i; k++) {
             if (choix == choisis[k]) {
