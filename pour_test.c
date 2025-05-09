ade04b1f642528a3bd8f80ec0c27fbd278a93810
SaigerMaine <pavel.gorenflot2006@gmail.com>
2025-05-09 17:08:25 +0200

    Update pour_test.c

--- pour_test.c
+++ pour_test.c
@@ -547,6 +547,18 @@ Character prochainperso(Jeu j){            // le prochain personnage a attaquer
     }
 }
 
+float calcul_des_dégats_pris(character p1,character p2){ //p1=perso attaqué, p2=perso qui attaque
+    int i=0;
+    i=(rand()%100)+1;
+    if(i<=p1.RES){
+        return p1.actHP;
+    }
+    else{
+        p1.actHP=p1.actHP-p2.ATK*(1-(p1.DEF/100));
+        return p1.actHP;
+    }
+}
+
 void ordifacile(){           //fonction pour jouer contre l'ordi mais c'est le debut et il faut mettre les bons perso dans le tableau 
     MakeTeam(les characters);
     MakeTeambot(les characters);
