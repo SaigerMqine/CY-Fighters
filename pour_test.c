b0bea9d8ac742930de5439825ae8bf0ba2c9bca2
SaigerMaine <pavel.gorenflot2006@gmail.com>
2025-05-09 16:58:24 +0200

    Update pour_test.c

--- pour_test.c
+++ pour_test.c
@@ -1,6 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+
+typedef struct{     // create class special skills
+    char name[50];
+    float value;
+    float duration;
+    float cd;
+    char description[100];
+}Skill;
+
+typedef struct{     // create class characters 
+    char name[50];
+    float MaxHP;
+    float ActHP;
+    float ATK;
+    float DEF;
+    float RES;
+    float SPD;
+    int ATK_bar;
+    Skill ch_skill1;
+    Skill ch_skill2;
+}Character;
+
+typedef struct{     // create class special skills
+    char name[50];
+    Character p1;
+    Character p2;
+    Character p3;
+}Team;
+
+typedef struct {
+    Team t1;
+    Team t2;
+} Jeu
 
 int verifint(){
     int d=0;
