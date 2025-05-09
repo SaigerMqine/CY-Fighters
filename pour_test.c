f74dcfef6f6d7ea44d487d8f315ce118012cfa55
SaigerMaine <pavel.gorenflot2006@gmail.com>
2025-05-09 17:00:44 +0200

    Update pour_test.c

--- pour_test.c
+++ pour_test.c
@@ -66,3 +66,18 @@ else {
 
     return name;
 }
+
+Character maxi(Character p1,Character p2){            // le perso avec la plus grande atk_bar
+    if(p1.ATK_bar>=p2.ATK_bar){
+        return p1;
+    }
+    else{
+        return p2;
+    }
+}
+
+Character prochainperso(Jeu j){            // le prochain personnage a attaquer
+    if(maxi(maxi(maxi(j.t1.p1,j.t1.p2),maxi(j.t1.p3,j.t2.p1)),maxi(j.t2.p2,j.t2.p3)).ATK_bar>=500){
+        return maxi(maxi(maxi(j.t1.p1,j.t1.p2),maxi(j.t1.p3,j.t2.p1)),maxi(j.t2.p2,j.t2.p3));
+    }
+}
