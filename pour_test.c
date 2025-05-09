88bfe7c49f33dea1ae6de55ffdd1b1a3e9e66902
SaigerMaine <pavel.gorenflot2006@gmail.com>
2025-05-09 17:09:20 +0200

    Update pour_test.c

--- pour_test.c
+++ pour_test.c
@@ -69,6 +69,20 @@ else {
     return name;
 }
 
+void afficher_perso(Character c, int index) {
+    printf(" ┌────────────────────────────────┐\n");
+    printf(" │ Personnage %d : %s │\n", index + 1, c.name);
+    printf(" ├────────────────────────────────┤\n");
+    printf(" │ HP  : %.1f │\n", c.MaxHP);
+    printf(" │ ATK : %.1f │\n", c.ATK);
+    printf(" │ DEF : %.1f │\n", c.DEF);
+    printf(" │ RES : %.1f │\n", c.RES);
+    printf(" │ SPD : %.1f │\n", c.SPD);
+    printf(" │ Skill 1 : %s │\n", c.ch_skill1.name);
+    printf(" │ Skill 2 : %s │\n", c.ch_skill2.name);
+    printf(" └────────────────────────────────┘\n");
+}
+
 Team MakeTeam(Character characters[], int nb) {
     Team team1;
     printf("Entrez le nom de votre équipe : ");
