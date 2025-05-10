f0bbbf36a8495fa33f1d5c4cdf7db9572bb4f09b
xotu6066 <aymericcasteret@orange.fr>
2025-05-10 11:33:59 +0200

    Update jeu_interface.c

--- jeu_interface.c
+++ jeu_interface.c
@@ -9,7 +9,7 @@
      else{
      printf(" |1|");
      }
-     for(int i=strlen(t1.p1.name) ;i<36 ;i++){
+     for(int i=strlen(t1.p1.name) ;i<32 ;i++){
         printf(" ");
     }
      printf("%s",t1.p2.name);
@@ -19,7 +19,7 @@
      else{
      printf(" |2|");
      }
-     for(int i=strlen(t1.p2.name) ;i<36 ;i++){
+     for(int i=strlen(t1.p2.name) ;i<32 ;i++){
         printf(" ");
     }
      printf("%s",t1.p3.name);
@@ -29,150 +29,149 @@
      else{
      printf(" |3|");
      }
-    for(int i=strlen(t1.p3.name) ;i<25 ;i++){
+    for(int i=strlen(t1.p3.name) ;i<20 ;i++){
         printf(" ");
     }
-     printf("|")
-     // faut la faire j'ai pas eu le temps entre les print faut mettre les bon nombre d'espaces 
+     printf("|");
  }
  
  void hpsp(Character p1,Character p2, Character p3 ){
      printf("| [ ");
-    for(int i=0; i<(p1.ActHp) ;i=i+25){
+    for(int i=0; i<(p1.ActHP) ;i=i+25){
     printf("#");
     }
-    for(int i=p1.ActHp ;i<=500 ;i=i+25){
+    for(int i=p1.ActHP ;i<=500 ;i=i+25){
         printf(" ");
     }
     printf("]");
     printf("            ");
     printf("[ ");
-    for(int i=0; i<(p2.ActHp) ;i=i+25){
+    for(int i=0; i<(p2.ActHP) ;i=i+25){
     printf("#");
     }
-    for(int i=p2.ActHp ;i<=500 ;i=i+25){
+    for(int i=p2.ActHP ;i<=500 ;i=i+25){
         printf(" ");
     }
     printf("]");
     printf("            ");
     printf("[ ");
-    for(int i=0; i<(p3.ActHp) ;i=i+25){
+    for(int i=0; i<(p3.ActHP) ;i=i+25){
     printf("#");
     }
-    for(int i=p3.ActHp ;i<=500 ;i=i+25){
+    for(int i=p3.ActHP ;i<=500 ;i=i+25){
         printf(" ");
     }
     printf("] |");
 
     printf("\n");
     printf("| [ ");
-    for(int i=0; i<(p1.Atk_bar) ;i=i+25){
+    for(int i=0; i<(p1.ATK_bar) ;i=i+25){
     printf(">");
     }
-    for(int i=p1.Atk_bar ;i<=500 ;i=i+25){
+    for(int i=p1.ATK_bar ;i<=500 ;i=i+25){
         printf(" ");
     }
     printf("]");
     printf("            ");
     printf("[ ");
-    for(int i=0; i<(p2.Atk_bar) ;i=i+25){
+    for(int i=0; i<(p2.ATK_bar) ;i=i+25){
     printf(">");
     }
-    for(int i=p2.Atk_bar  ;i<=500 ;i=i+25){
+    for(int i=p2.ATK_bar  ;i<=500 ;i=i+25){
         printf(" ");
     }
     printf("]");
     printf("            ");
     printf("[ ");
-    for(int i=0; i<(p3.Atk_bar) ;i=i+25){
+    for(int i=0; i<(p3.ATK_bar) ;i=i+25){
     printf(">");
     }
-    for(int i=p3.Atk_bar  ;i<=500 ;i=i+25){
+    for(int i=p3.ATK_bar  ;i<=500 ;i=i+25){
         printf(" ");
     }
     printf("] |");
  }
  
- void afficheeffet(){
+ void affiche_effet(){
      // il faut le faire
  }
 
   
- void afficheperso(character p1){
+ void afficheperso(Character p1){
+     printf("\n");
      printf("|   %s",p1.name);
-     for(int i=strlen(p1.name);i<95-;i++){
+     for(int i=strlen(p1.name);i<94;i++){
          printf(" ");
      }
-     printf("|\n");
+     printf(" |\n");
      printf("|   TECHNIQUES SPECIALES");
-     for(int i=0;i<75-;i++){
+     for(int i=0;i<75;i++){
          printf(" ");
      }
      printf("|\n");
-     printf("|      |1| attaque normal(peut etre utiliser a chaque tour)                                            |");
+     printf("|      |1| attaque normal(peut etre utiliser a chaque tour)                                        |");
+      printf("\n");
      if (p1.ch_skill1.cd == 0){
          printf("|      |2|");
-         printf(" %s (duree:%d tours)",p1.ch_skill1.name ,p1.ch_skill1.duration);
-         for(int i=strlen(p1.ch_skill1.name);i<73;i++){
+         printf(" %s (duree:%f tours)",p1.ch_skill1.name ,p1.ch_skill1.duration);
+         for(int i=strlen(p1.ch_skill1.name);i<58;i++){
          printf(" ");
         }
         printf("|\n");
      }
      else{
          printf("|      |-|");
-         printf(" %s (recharge: %d duree:%d tours)",p1.ch_skill2.name,p1.ch_skill2.cd ,p1.ch_skill2.duration);
-         for(int i=strlen(p1.ch_skill1.name);i<61;i++){
+         printf(" %s (recharge: %f duree:%f tours)",p1.ch_skill1.name,p1.ch_skill1.cd ,p1.ch_skill2.duration);
+         for(int i=strlen(p1.ch_skill1.name);i<46;i++){
          printf(" ");
         }
         printf("|\n");
      }
      printf("|            %s",p1.ch_skill1.description);
-     for(int i=strlen(p1.ch_skill1.description);i<87;i++){
+     for(int i=strlen(p1.ch_skill1.description);i<86;i++){
          printf(" ");
     }
      printf("|\n");
      
           if (p1.ch_skill2.cd == 0){
          printf("|      |3|");
-         printf(" %s (duree:%d tours)",p1.ch_skill2.name ,p1.ch_skill2.duration);
-         for(int i=strlen(p1.ch_skill2.name);i<73;i++){
+         printf(" %s (duree:%f tours)",p1.ch_skill2.name ,p1.ch_skill2.duration);
+         for(int i=strlen(p1.ch_skill2.name);i<58;i++){
          printf(" ");
         }
         printf("|\n");
      }
      else{
          printf("|      |-|");
-         printf(" %s (recharge: %d duree:%d tours)",p1.ch_skill2.name,p1.ch_skill2.cd ,p1.ch_skill2.duration);
-         for(int i=strlen(p1.ch_skill2.name);i<61;i++){
+         printf(" %s (recharge: %f duree:%f tours)",p1.ch_skill2.name,p1.ch_skill2.cd ,p1.ch_skill2.duration);
+         for(int i=strlen(p1.ch_skill2.name);i<46;i++){
              printf(" ");
         }
         printf("|\n");
      }
       printf("|            %s",p1.ch_skill1.description);
-     for(int i=strlen(p1.ch_skill1.description);i<87;i++){
+     for(int i=strlen(p1.ch_skill2.description);i<86;i++){
          printf(" ");
     }
-     printf("|\n");
+     printf("|");
      
  }
 
- 
  void affichageteam(Team t1){
-     printf("┌──[%s]",t1.name);// il faut faire les 2 team
-     for(int i=strlen(t1.name);i<94-;i++){
+     printf("┌──[");
+    for(int i=0; i<strlen(t1.name)-1;i++){
+    printf("%c",t1.name[i]);
+    }
+    printf("]");
+ 
+     for(int i=strlen(t1.name);i<95;i++){
          printf("─");
      }
      printf("┐");
-     printf("\n| ");
+     printf("\n");
      affichenom(t1);
      printf("\n");
-     afficheeffet();
      hpsp(t1.p1,t1.p2,t1.p3);
-     printf("\n|");
-     for (int i=0;i<98;i++){
-         printf(" ");
-     }
-     printf("|");
  }
  void affichagefin(){
      printf("\n|");
@@ -196,14 +195,15 @@ void affichage1v1(Jeu j){ // fonction prochainperso a code il faut qu'elle renvo
          affichagefin();
          printf("\n \n");
          printf("┌");
-         for(int i=0;i<99;i++){
+         for(int i=0;i<98;i++){
              printf("─");
          }
          printf("┐");
          printf("\n| ");
-         for(int i=0;i<99;i++){
+         for(int i=0;i<97;i++){
              printf(" ");
          }
+         printf("|");
          afficheperso(j.t1.p1);
          affichagefin();
          
@@ -217,14 +217,15 @@ void affichage1v1(Jeu j){ // fonction prochainperso a code il faut qu'elle renvo
         affichagefin();
          printf("\n \n");
          printf("┌");
-         for(int i=0;i<99;i++){
+         for(int i=0;i<98;i++){
              printf("─");
          }
          printf("┐");
          printf("\n| ");
-         for(int i=0;i<99;i++){
+         for(int i=0;i<97;i++){
              printf(" ");
          }
+         printf("|");
          afficheperso(j.t1.p2);
          affichagefin();
          
@@ -238,14 +239,15 @@ void affichage1v1(Jeu j){ // fonction prochainperso a code il faut qu'elle renvo
          affichagefin();
          printf("\n \n");
          printf("┌");
-         for(int i=0;i<99;i++){
+         for(int i=0;i<98;i++){
              printf("─");
          }
          printf("┐");
          printf("\n| ");
-         for(int i=0;i<99;i++){
+         for(int i=0;i<97;i++){
              printf(" ");
          }
+         printf("|");
          afficheperso(j.t1.p3);
          affichagefin();
          
@@ -259,14 +261,15 @@ void affichage1v1(Jeu j){ // fonction prochainperso a code il faut qu'elle renvo
          affichagefin();
          printf("\n \n");
          printf("┌");
-         for(int i=0;i<99;i++){
+         for(int i=0;i<98;i++){
              printf("─");
          }
          printf("┐");
          printf("\n| ");
-         for(int i=0;i<99;i++){
+         for(int i=0;i<97;i++){
              printf(" ");
          }
+         printf("|");
          afficheperso(j.t2.p1);
          affichagefin();
          
@@ -280,14 +283,15 @@ void affichage1v1(Jeu j){ // fonction prochainperso a code il faut qu'elle renvo
          affichagefin();
          printf("\n \n");
          printf("┌");
-         for(int i=0;i<99;i++){
+         for(int i=0;i<98;i++){
              printf("─");
          }
          printf("┐");
          printf("\n| ");
-         for(int i=0;i<99;i++){
+         for(int i=0;i<97;i++){
              printf(" ");
          }
+         printf("|");
          afficheperso(j.t2.p2);
          affichagefin();
          
@@ -300,14 +304,15 @@ void affichage1v1(Jeu j){ // fonction prochainperso a code il faut qu'elle renvo
          affichagefin();
          printf("\n \n");
          printf("┌");
-         for(int i=0;i<99;i++){
+         for(int i=0;i<98;i++){
              printf("─");
          }
          printf("┐");
          printf("\n| ");
-         for(int i=0;i<99;i++){
+         for(int i=0;i<97;i++){
              printf(" ");
          }
+         printf("|");
          afficheperso(j.t2.p3);
          affichagefin();
          
