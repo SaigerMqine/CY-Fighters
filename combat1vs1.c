7a6e16930ab61c0bfe5c424bf9ece9bc75edbdd6
timothee23-gh <timothee.renouf23@gmail.com>
2025-05-10 12:12:40 +0200

    Update combat1vs1.c

--- combat1vs1.c
+++ combat1vs1.c
@@ -13,12 +13,12 @@ Character tab2[3];
     tab2[1]=jeu.t2.p2;
     tab2[2]=jeu.t2.p3;
     while(jeu.t1.p1.ActHP > 0 && jeu.t1.p2.ActHP > 0 && jeu.t1.p3.ActHP > 0 || jeu.t2.p1.ActHP > 0 && jeu.t2.p2.ActHP > 0 && jeu.t2.p3.ActHP > 0){
-        Team1.p1.ATK_bar=Team1.p1.ATK_bar+Team1.p1.SPD;
-        Team1.p2.ATK_bar=Team1.p2.ATK_bar+Team1.p2.SPD;
-        Team1.p3.ATK_bar=Team1.p3.ATK_bar+Team1.p3.SPD;
-        Team2.p1.ATK_bar=Team2.p1.ATK_bar+Team2.p1.SPD;
-        Team2.p2.ATK_bar=Team2.p2.ATK_bar+Team2.p2.SPD;
-        Team2.p3.ATK_bar=Team2.p3.ATK_bar+Team2.p3.SPD;
+        j.t1.p1.ATK_bar=j.t1.p1.ATK_bar+j.t1.p1.SPD;
+        j.t1.p2.ATK_bar=j.t1.p2.ATK_bar+j.t1.p2.SPD;
+        j.t1.p3.ATK_bar=j.t1.p3.ATK_bar+j.t1.p3.SPD;
+        j.t2.p1.ATK_bar=j.t2.p1.ATK_bar+j.t2.p1.SPD;
+        j.t2.p2.ATK_bar=j.t2.p2.ATK_bar+j.t2.p2.SPD;
+        j.t2.p3.ATK_bar=j.t2.p3.ATK_bar+j.t2.p3.SPD;
         if(jeu.t1.p1.ActHP > 500 || jeu.t1.p2.ActHP > 500 || jeu.t1.p3.ActHP > 500 || jeu.t2.p1.ActHP > 500 || jeu.t2.p2.ActHP > 500 || jeu.t2.p3.ActHP > 500){
             affichage1vs1();
             printf("quelle capacité voulez vous utiliser");
@@ -26,7 +26,7 @@ Character tab2[3];
             if(n==1){
                 printf("quelle personnage voulez vous cibler");
                 d=verifint();
-                calcul_des_dégats_pris(prochainperso(),tab[d])
+                calcul_des_dégats_pris(prochainperso(&j),tab[d]);
         }
     }   
 }
