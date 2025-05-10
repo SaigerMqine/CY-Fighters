561690e9234955f259e1018f375971a1f733b960
SaigerMaine <pavel.gorenflot2006@gmail.com>
2025-05-10 08:38:59 +0200

    créa compare perso

---
+++ compare_perso.c
@@ -0,0 +1,12 @@
+int compare_perso(Character c1, Character c2) {
+    if (c1.name != c2.name) {
+        return 0;
+    }
+    else {
+        return 1;
+    }
+}
+
+bool compare_perso_team(Character c1, Character c2){
+    
+}
