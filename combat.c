dc072394d0be7e40ab126aed9e9c7a41e6c8afde
xotu6066 <aymericcasteret@orange.fr>
2025-05-10 12:39:24 +0200

    Update combat.c

--- combat.c
+++ combat.c
@@ -1,20 +1,84 @@
-void ordifacile(){           //fonction pour jouer contre l'ordi mais c'est le debut et il faut mettre les bons perso dans le tableau 
-    MakeTeam(les characters);
-    MakeTeambot(les characters);
-    while(Team1.p1.ActHP > 0 && Team1.p2.ActHP > 0 && Team1.p3.ActHP > 0 || Team2.p1.ActHP > 0 && Team2.p2.ActHP > 0 && Team2.p3.ActHP > 0){
+
+void botfacile(){
+    Jeu jeu;
+    int n=0;
+    int d=0;
+    Character tab1[3];
+    Character tab2[3];
+    jeu.t1 = MakeTeam(tabC)
+    jeu.t2 = MakeTeambot(tabC);
+    tab1[0]=jeu.t1.p1;
+    tab1[1]=jeu.t1.p2;
+    tab1[2]=jeu.t1.p3;
+    tab2[0]=jeu.t2.p1;
+    tab2[1]=jeu.t2.p2;
+    tab2[2]=jeu.t2.p3;
+    while(jeu.t1.p1.ActHP > 0 && jeu.t1.p2.ActHP > 0 && jeu.t1.p3.ActHP > 0 || jeu.t2.p1.ActHP > 0 && jeu.t2.p2.ActHP > 0 && jeu.t2.p3.ActHP > 0){
         Team1.p1.ATK_bar=Team1.p1.ATK_bar+Team1.p1.SPD;
         Team1.p2.ATK_bar=Team1.p2.ATK_bar+Team1.p2.SPD;
         Team1.p3.ATK_bar=Team1.p3.ATK_bar+Team1.p3.SPD;
         Team2.p1.ATK_bar=Team2.p1.ATK_bar+Team2.p1.SPD;
         Team2.p2.ATK_bar=Team2.p2.ATK_bar+Team2.p2.SPD;
         Team2.p3.ATK_bar=Team2.p3.ATK_bar+Team2.p3.SPD;
-                if (Team1.p1.ATK_bar >=500 ||   Team1.p2.ATK_bar >= 500 || Team1.p3.ATK_bar >= 500 || Team2.p1.ATK_bar >= 500 || Team2.p2.ATK_bar >= 500 || Team2.p3.ATK_bar >= 500){
-                
-                    
-                    AffichageDuPerso()   // dans l'affichage on met aussi les verifint pour choix de l'atk
-                
-                }
-            }      
+        if(jeu.t1.p1.ActHP >= 500 || jeu.t1.p2.ActHP >= 500 || jeu.t1.p3.ActHP >= 500){
+            affichage1vs1(jeu);
+            printf("quelle capacité voulez vous utiliser");
+            n=verifint();
+            if(n==1){
+                printf("quelle personnage voulez vous cibler");
+                d=verifint() -1;
+               calcul_des_dégats_pris(prochainperso(&jeu),tab2[d]);
+            }
         }
+        else if(jeu.t2.p1.ActHP >= 500 || jeu.t2.p2.ActHP >= 500 || jeu.t2.p3.ActHP >= 500){
+            affichage1vs1(jeu);
+            calcul_des_dégats_pris(prochainperso(&jeu),tab[(rand()%3)+1]);
     }
-}// je vous laisse l'ameliorer normalement le debut sera toujours pareil pour chaque ordi et pour le 1 contre 1 il faudra juste changer ChoixOrdiEquipe aussi j ai mis des fonction mais je sais pas quelle nom vous voulez mettre 
+    
+    
+    
+    
+void botmoyen(){
+    Jeu jeu;
+    int n=0;
+    int d=0;
+    Character tab1[3];
+    Character tab2[3];
+    jeu.t1 = MakeTeam(tabC)
+    jeu.t2 = MakeTeambot(tabC);
+    tab1[0]=jeu.t1.p1;
+    tab1[1]=jeu.t1.p2;
+    tab1[2]=jeu.t1.p3;
+    tab2[0]=jeu.t2.p1;
+    tab2[1]=jeu.t2.p2;
+    tab2[2]=jeu.t2.p3;
+    while(jeu.t1.p1.ActHP > 0 && jeu.t1.p2.ActHP > 0 && jeu.t1.p3.ActHP > 0 || jeu.t2.p1.ActHP > 0 && jeu.t2.p2.ActHP > 0 && jeu.t2.p3.ActHP > 0){
+        Team1.p1.ATK_bar=Team1.p1.ATK_bar+Team1.p1.SPD;
+        Team1.p2.ATK_bar=Team1.p2.ATK_bar+Team1.p2.SPD;
+        Team1.p3.ATK_bar=Team1.p3.ATK_bar+Team1.p3.SPD;
+        Team2.p1.ATK_bar=Team2.p1.ATK_bar+Team2.p1.SPD;
+        Team2.p2.ATK_bar=Team2.p2.ATK_bar+Team2.p2.SPD;
+        Team2.p3.ATK_bar=Team2.p3.ATK_bar+Team2.p3.SPD;
+        if(jeu.t1.p1.ActHP >= 500 || jeu.t1.p2.ActHP >= 500 || jeu.t1.p3.ActHP >= 500){
+            affichage1vs1(jeu);
+            printf("quelle capacité voulez vous utiliser");
+            n=verifint();
+            if(n==1){
+                printf("quelle personnage voulez vous cibler");
+                d=verifint() -1;
+               calcul_des_dégats_pris(prochainperso(&jeu),tab2[d]);
+            }
+        }
+        else if(jeu.t2.p1.ActHP >= 500 || jeu.t2.p2.ActHP >= 500 || jeu.t2.p3.ActHP >= 500){
+            affichage1vs1(jeu);
+            if(tab[0].ActHP>=tab[1].ActHP&&tab[0].ActHP>=tab[2].ActHP){
+            calcul_des_dégats_pris(prochainperso(&jeu),tab[0]);
+            }
+            else if(tab[1].ActHP>=tab[0].ActHP&&tab[1].ActHP>=tab[2].ActHP){
+            calcul_des_dégats_pris(prochainperso(&jeu),tab[1]);
+            }
+            else if(tab[2].ActHP>=tab[1].ActHP&&tab[2].ActHP>=tab[0].ActHP){
+            calcul_des_dégats_pris(prochainperso(&jeu),tab[2]);
+            }
+    }
+       
