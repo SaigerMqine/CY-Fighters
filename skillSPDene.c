7ad86233f2f76d17d4c167bcc855783028c8b235
xotu6066 <aymericcasteret@orange.fr>
2025-05-11 22:45:46 +0200

    Update skillSPDene.c

--- skillSPDene.c
+++ skillSPDene.c
@@ -2,6 +2,6 @@ float skillSPDene (character* c1,character c2){
   c2.SPD=c2.SPD/c1->Skill2.val;
   c1->skill2.dcd=c1->skill2.cd;
 }
-float antiskillSPDene (character c1,character c2){
-  c2.SPD=c2.SPD*c1.Skill2.val;
+float antiskillSPDene (character* c1,character c2){
+  c2.SPD=c2.SPD*c1->Skill2.val;
 }
