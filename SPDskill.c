c7ac860a1298927d9fddf2bdb29311b1dcbd8cb2
timothee23-gh <timothee.renouf23@gmail.com>
2025-05-11 12:59:50 +0200

    Create SPDskill.c

---
+++ SPDskill.c
@@ -0,0 +1,7 @@
+float skillSPD (character* c1,character c2){
+  c2.SPD=c2.SPD+((c2.SPD*c1->Skill.val)/100);
+  c1->skill.dcd=c1->skill.cd;
+}
+float antiskillATK (character* c1,character c2){
+  c2.SPD=c2.SPD-((c2.SPD*c1->Skill.val)/100);
+}
