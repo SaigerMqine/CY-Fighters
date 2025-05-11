8740c628fe18532c6049c99ce1682a2b0686f664
timothee23-gh <timothee.renouf23@gmail.com>
2025-05-11 21:39:33 +0200

    Update skillDEFene.c

--- skillDEFene.c
+++ skillDEFene.c
@@ -3,5 +3,5 @@ float skillSPDene (character* c1,character c2){
   c1->skill2.dcd=c1->skill2.cd;
 }
 float antiskillSPDene (character* c1,character c2){
-  c2.SPD=c2.SPD*c1->Skill2.val;
+  c2.SPD=c2.SPD*c1.Skill2.val;
 }
