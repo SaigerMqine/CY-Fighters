01d9d5d0ebde2c622413a73bc95fee5e66d9a299
xotu6066 <aymericcasteret@orange.fr>
2025-05-11 23:02:20 +0200

    Update skillSPD.c

--- skillSPD.c
+++ skillSPD.c
@@ -1,9 +1,9 @@
-float skillSPD (character* c1,character c2){
+float skillSPD (character* c1,character c2){      //buff SPD
   c2.SPD=c2.SPD*c1->Skill1.val;
   c1->skill1.dcd=c1->skill1.cd;
   return c2.SPD
 }
-float antiskillSPD (character* c1,character c2){
+float antiskillSPD (character* c1,character c2){    // antibuff SPD
   c2.SPD=c2.SPD/c1->Skill1.val;
   return c2.SPD
 }
