e17c4549163c10c0bb38209f0c52bbf6ca5261c1
timothee23-gh <timothee.renouf23@gmail.com>
2025-05-11 22:43:30 +0200

    Update skillDEF.c

--- skillDEF.c
+++ skillDEF.c
@@ -2,6 +2,6 @@ float skillDEF (character* c1,character c2){
   c2.DEF=c2.DEF*c1->Skill2.val;
   c1->skill2.dcd=c1->skill2.cd;
 }
-float antiskillDEF (character c1,character c2){
-  c2.DEF=c2.DEF/c1.Skill2.val;
+float antiskillDEF (character* c1,character c2){
+  c2.DEF=c2.DEF/c1->Skill2.val;
 }
