0e683d80c9618067e9d72a3b057870c01ac0d8dc
xotu6066 <aymericcasteret@orange.fr>
2025-05-11 22:44:55 +0200

    Update skillRES.c

--- skillRES.c
+++ skillRES.c
@@ -2,6 +2,6 @@ float skillRES (character* c1,character c2){
   c2.RES=c2.RES*c1->Skill2.val;
   c1->skill2.dcd=c1->skill2.cd;
 }
-float antiskillRES (character c1,character c2){
-  c2.RES=c2.RES/c1.Skill2.val;
+float antiskillRES (character* c1,character c2){
+  c2.RES=c2.RES/c1->Skill2.val;
 }
