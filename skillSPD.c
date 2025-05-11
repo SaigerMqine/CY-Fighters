62c0ae78c87462815ad7461d079b8e549caf0004
xotu6066 <aymericcasteret@orange.fr>
2025-05-11 22:54:40 +0200

    Update skillSPD.c

--- skillSPD.c
+++ skillSPD.c
@@ -1,7 +1,9 @@
 float skillSPD (character* c1,character c2){
   c2.SPD=c2.SPD*c1->Skill1.val;
   c1->skill1.dcd=c1->skill1.cd;
+  return c2.SPD
 }
-float antiskillATK (character* c1,character c2){
+float antiskillSPD (character* c1,character c2){
   c2.SPD=c2.SPD/c1->Skill1.val;
+  return c2.SPD
 }
