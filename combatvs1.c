7b5d62116637e17038fa118ee1c1591efb73c8ef
xotu6066 <aymericcasteret@orange.fr>
2025-05-11 22:27:12 +0200

    Update combatvs1.c

--- combatvs1.c
+++ combatvs1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-void vs1(Character tabC[],Jeu* j){
-int n=0;
-int d=0;
+void vs1(Character tabC[],Jeu* j){            //combat joueur contre joueur
+int n=0;            //quelle attaque choisir
+int d=0;            //quel perso cibler
 Character tab1[3];
 Character tab2[3];
     jeu.t1=MakeTeam(tabC,6);
@@ -12,7 +12,7 @@ Character tab2[3];
     tab2[0]=jeu.t2.p1;
     tab2[1]=jeu.t2.p2;
     tab2[2]=jeu.t2.p3;
-    while(jeu.t1.p1.ActHP > 0 && jeu.t1.p2.ActHP > 0 && jeu.t1.p3.ActHP > 0 || jeu.t2.p1.ActHP > 0 && jeu.t2.p2.ActHP > 0 && jeu.t2.p3.ActHP > 0){
+    while(jeu.t1.p1.ActHP > 0 && jeu.t1.p2.ActHP > 0 && jeu.t1.p3.ActHP > 0 || jeu.t2.p1.ActHP > 0 && jeu.t2.p2.ActHP > 0 && jeu.t2.p3.ActHP > 0){            //condition d'arret du jeu 
         jeu.t1.p1.ATK_bar=jeu.t1.p1.ATK_bar+jeu.t1.p1.SPD;
         jeu.t1.p2.ATK_bar=jeu.t1.p2.ATK_bar+jeu.t1.p2.SPD;
         jeu.t1.p3.ATK_bar=jeu.t1.p3.ATK_bar+jeu.t1.p3.SPD;
@@ -24,7 +24,10 @@ Character tab2[3];
                 affichage1v1(jeu);
             printf("\n quelle capacité voulez vous utiliser");
             n=verifint();
-            if(n==1){
+                    while(n=!1||n=!2||n=!3){            //verifier n
+                        n=verifint();
+                    }
+            if(n==1){            //attaque normale
                 printf("\n quelle personnage voulez vous cibler");
                 d=verifint() -1;
                 while (tab2[d].ActHP<=0){
@@ -44,14 +47,14 @@ Character tab2[3];
 
                }
             }
-                    else if(n==2){
+                    else if(n==2){         //attaque skill 1
                     
                 }
-                else if(n==3){
+                else if(n==3){            //attaque skill 2
                     
                     }
             }
-            if(&jeu.t1.p1 == prochainperso(&jeu)){
+            if(&jeu.t1.p1 == prochainperso(&jeu)){            //remise a 0 de atk bar
                 jeu.t1.p1.ATK_bar =0;
             }
             if(&jeu.t1.p2 == prochainperso(&jeu)){
@@ -60,10 +63,13 @@ Character tab2[3];
             if(&jeu.t1.p3 == prochainperso(&jeu)){
                 jeu.t1.p3.ATK_bar =0;
             }
-                else {
+                else {            //2eme joueur avec le meme fonctionnement
                 affichage1v1(jeu);
             printf("\n quelle capacité voulez vous utiliser");
             n=verifint();
+                    while(n=!1||n=!2||n=!3){          
+                        n=verifint();
+                    }
             if(n==1){
                 printf("\n quelle personnage voulez vous cibler");
                 d=verifint() -1;
@@ -102,7 +108,7 @@ Character tab2[3];
             }
             
            
-           if(jeu.t1.p1.ActHP <= 0){
+           if(jeu.t1.p1.ActHP <= 0){            //quand le perso est mort
         jeu.t1.p1.SPD=0;
         jeu.t1.p1.ATK_bar=0;
            }
