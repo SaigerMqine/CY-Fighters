2bf2515854bb3474749bbc46763942b4ddf23a28
timothee23-gh <timothee.renouf23@gmail.com>
2025-05-11 18:54:51 +0200

    Update skillATK.c

--- skillATK.c
+++ skillATK.c
@@ -1,9 +1,9 @@
 float skillATK (character* c1,character c2){
-  c2.ATK=c2.ATK*c1->Skill.val;
-  c1->skill.dcd=c1->skill.cd;
+  c2.ATK=c2.ATK*c1->Skill1.val;
+  c1->skill1.dcd=c1->skill1.cd;
   return c2.ATK;
 }
 float antiskillATK (character* c1,character c2){
-  c2.ATK=c2.ATK/c1->Skill.val;
+  c2.ATK=c2.ATK/c1->Skill1.val;
   return c2.ATK;
 }
