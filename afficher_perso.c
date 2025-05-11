cf9fe43fc7f905b2769d4221786245989a77f914
xotu6066 <aymericcasteret@orange.fr>
2025-05-11 20:03:43 +0200

    Update afficher_perso.c

--- afficher_perso.c
+++ afficher_perso.c
@@ -1,13 +1,26 @@
+
 void afficher_perso(Character c, int index) {
     printf(" ┌────────────────────────────────┐\n");
-    printf(" │ Personnage %d : %s │\n", index + 1, c.name);
+    printf(" │ Personnage %d : %s ", index + 1, c.name);
+    for(int i=strlen(c.name);i<21;i++){
+         printf(" ");
+     }
+     printf("│\n");
     printf(" ├────────────────────────────────┤\n");
-    printf(" │ HP  : %.1f │\n", c.MaxHP);
-    printf(" │ ATK : %.1f │\n", c.ATK);
-    printf(" │ DEF : %.1f │\n", c.DEF);
-    printf(" │ RES : %.1f │\n", c.RES);
-    printf(" │ SPD : %.1f │\n", c.SPD);
-    printf(" │ Skill 1 : %s │\n", c.ch_skill1.name);
-    printf(" │ Skill 2 : %s │\n", c.ch_skill2.name);
+    printf(" │ HP  : %.1f                          │\n", c.MaxHP);
+    printf(" │ ATK : %.1f                           │\n", c.ATK);
+    printf(" │ DEF : %.1f                           │\n", c.DEF);
+    printf(" │ RES : %.1f                           │\n", c.RES);
+    printf(" │ SPD : %.1f                           │\n", c.SPD);
+    printf(" │ Skill 1 : %s", c.ch_skill1.name);
+    for(int i=strlen(c.ch_skill1.name);i<27;i++){
+         printf(" ");
+     }
+     printf("|\n");
+    printf(" │ Skill 2 : %s", c.ch_skill2.name);
+    for(int i=strlen(c.ch_skill2.name);i<27;i++){
+         printf(" ");
+     }
+     printf("|\n");
     printf(" └────────────────────────────────┘\n");
 }
