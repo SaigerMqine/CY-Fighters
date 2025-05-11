60e322adba71fc93f36e2ec3b581e35c03aef8a4
timothee23-gh <timothee.renouf23@gmail.com>
2025-05-11 18:55:21 +0200

    Update skillSPD.c

--- skillSPD.c
+++ skillSPD.c
@@ -1,7 +1,7 @@
 float skillSPD (character* c1,character c2){
-  c2.SPD=c2.SPD*c1->Skill.val;
-  c1->skill.dcd=c1->skill.cd;
+  c2.SPD=c2.SPD*c1->Skill1.val;
+  c1->skill1.dcd=c1->skill1.cd;
 }
 float antiskillATK (character* c1,character c2){
-  c2.SPD=c2.SPD/c1->Skill.val;
+  c2.SPD=c2.SPD/c1->Skill1.val;
 }
