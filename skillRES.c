4af4008653eb18df88f71e054d70920dead6010f
timothee23-gh <timothee.renouf23@gmail.com>
2025-05-11 13:12:11 +0200

    Update skillRES.c

--- skillRES.c
+++ skillRES.c
@@ -1,7 +1,7 @@
 float skillRES (character* c1,character c2){
-  c2.RES=c2.RES+((c2.RES*c1->Skill.val)/100);
+  c2.RES=c2.RES*c1->Skill.val;
   c1->skill.dcd=c1->skill.cd;
 }
 float antiskillRES (character* c1,character c2){
-  c2.RES=c2.RES-((c2.RES*c1->Skill.val)/100);
+  c2.RES=c2.RES/c1->Skill.val;
 }
