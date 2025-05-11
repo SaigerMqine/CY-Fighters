99abad41fe1e0f01875c0cbcc2c81353f9fe6a91
xotu6066 <aymericcasteret@orange.fr>
2025-05-11 23:01:09 +0200

    Update skillSPDene.c

--- skillSPDene.c
+++ skillSPDene.c
@@ -1,9 +1,9 @@
-float skillSPDene (character* c1,character c2){
+float skillSPDene (character* c1,character c2){      //debuff SPD
   c2.SPD=c2.SPD/c1->Skill2.val;
   c1->skill2.dcd=c1->skill2.cd;
   return c2.SPD
 }
-float antiskillSPDene (character* c1,character c2){
+float antiskillSPDene (character* c1,character c2){      //antiDEBUFF SDP
   c2.SPD=c2.SPD*c1->Skill2.val;
   return c2.SPD
 }
