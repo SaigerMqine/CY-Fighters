e925bb5512b143c8cc5ca32cc5bbd952827fa1a7
xotu6066 <aymericcasteret@orange.fr>
2025-05-11 22:47:45 +0200

    Update skillDEF.c

--- skillDEF.c
+++ skillDEF.c
@@ -1,7 +1,9 @@
 float skillDEF (character* c1,character c2){
   c2.DEF=c2.DEF*c1->Skill2.val;
   c1->skill2.dcd=c1->skill2.cd;
+  return c2.DEF
 }
 float antiskillDEF (character* c1,character c2){
   c2.DEF=c2.DEF/c1->Skill2.val;
+  return c2.DEF
 }
